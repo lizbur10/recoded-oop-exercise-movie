1c7585cb6178056b3ac836459b52f4932b31f6f5
lizbur10 <liz.burton147@gmail.com>
2020-02-17 15:17:43 +0300

    added autorun, movie; started actor

--- script.js
+++ script.js
@@ -1,53 +1,39 @@
-const TMDB_BASE_URL = 'https://api.themoviedb.org/3'
-const PROFILE_BASE_URL = 'http://image.tmdb.org/t/p/w185'
-const BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780'
+const TMDB_BASE_URL = 'https://api.themoviedb.org/3' // full url for movie add 'movie/<movieId>?api_key=542003918769df50083a13c415bbc602'
+                                                      // full url for actors add 'movie/${movieId}/credits?api_key=542003918769df50083a13c415bbc602'
+const PROFILE_BASE_URL = 'http://image.tmdb.org/t/p/w185' 
+const BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780' // full url add '/<backdrop_path>' (backdrop_path from JSON)
 
-class App {
-  static run() {
-    APIService.fetchMovie(534)
-      .then(movie => Page.renderMovie(movie))
-  }
+document.addEventListener('DOMContentLoaded', autorun);
+
+function autorun() {
+  const movieId = 534;
+  const apiKey = '542003918769df50083a13c415bbc602';
+  fetch(`${TMDB_BASE_URL}/movie/${movieId}?api_key=${apiKey}`)
+    .then(resp => resp.json())
+    .then(json => {
+      renderMovie(json);
+      fetch(`${TMDB_BASE_URL}/movie/${movieId}/credits?api_key=${apiKey}`)
+        .then(actorResp => actorResp.json())
+        .then(actorJson => renderActors(actorJson.cast))
+    })
 }
 
-class APIService {
+function renderMovie(movie) {
+  const movieBackdrop = document.getElementById('movie-backdrop');
+  const movieTitle = document.getElementById('movie-title');
+  const movieReleaseDate = document.getElementById('movie-release-date');
+  const movieRuntime = document.getElementById('movie-runtime');
+  const movieOverview = document.getElementById('movie-overview');
 
-  static fetchMovie(movieId) {
-    const url = APIService._constructUrl(`movie/${movieId}`)
-    return fetch(url)
-      .then(res => res.json())
-      .then(json => new Movie(json))
-  }
-
-  static  _constructUrl(path) {
-    return `${TMDB_BASE_URL}/${path}?api_key=${atob('NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=')}`
-  }
+  movieBackdrop.src = `${BACKDROP_BASE_URL}/${movie.backdrop_path}`;
+  movieTitle.innerHTML = movie.title;
+  movieReleaseDate.innerHTML = movie.release_date;
+  movieRuntime.innerHTML = movie.runtime;
+  movieOverview.innerHTML = movie.overview;
 }
 
-class Page {
-  static backdrop = document.getElementById('movie-backdrop')
-  static title = document.getElementById('movie-title')
-  static releaseDate = document.getElementById('movie-release-date')
-  static runtime = document.getElementById('movie-runtime')
-  static overview = document.getElementById('movie-overview')
-
-  static renderMovie(movie) {
-    Page.backdrop.src = BACKDROP_BASE_URL + movie.backdropPath
-    Page.title.innerText = movie.title
-    Page.releaseDate.innerText = movie.releaseDate
-    Page.runtime.innerText = movie.runtime + " minutes"
-    Page.overview.innerText = movie.overview
-  }
+function renderActors(actors) {
+  actors.forEach(actor => {
+    console.log(actor);
+  })
 }
-
-class Movie {
-  constructor(json) {
-    this.id = json.id
-    this.title = json.title
-    this.releaseDate = json.release_date
-    this.runtime = json.runtime
-    this.overview = json.overview
-    this.backdropPath = json.backdrop_path
-  }
-}
-
-document.addEventListener("DOMContentLoaded", App.run);
