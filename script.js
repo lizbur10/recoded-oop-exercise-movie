a0f989abcbce72350ccdd695893e8367ddbdc420
lizbur10 <liz.burton147@gmail.com>
2020-02-18 12:45:53 +0300

    minor changes

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-const TMDB_BASE_URL = 'https://api.themoviedb.org/3'        // full url for movie add 'movie/<movieId>?api_key=542003918769df50083a13c415bbc602'
-                                                            // full url for actors add 'movie/${movieId}/credits?api_key=542003918769df50083a13c415bbc602'
-const PROFILE_BASE_URL = 'http://image.tmdb.org/t/p/w185'   // full url add '/<profile_path>' (from actor JSON)
-const BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780'  // full url add '/<backdrop_path>' (from movie JSON)
+//CLASSES:
+//  APIService - takes care of fetches and related functions
+//  Movie - creates movie instance with needed attributes (needs a constructor)
+//  Actor - creates actor instances with needed attributes (needs a constructor)
+//  Page - Has a section for movie and actors and calls the corresponding methods 
+//  MovieSection - grabs the needed html elements and renders the movie on the page 
+    // Will include a static render method
+//  ActorsSection - Takes the first four actors and passes them to RenderActor
+    // Will include two static render methods
 
 document.addEventListener('DOMContentLoaded', autorun);
 
@@ -17,16 +22,6 @@ function autorun() {
     .then(actors => Page.renderActorsSection(actors))
 }
 
-//CLASSES:
-//  APIService - takes care of fetches and related functions
-//  Movie - creates movie instance with needed attributes (needs a constructor)
-//  Actor - creates actor instances with needed attributes (needs a constructor)
-//  Page - Has a section for movie and actors and calls the corresponding methods 
-//  MovieSection - grabs the needed html elements and renders the movie on the page 
-    // Will include a static render method
-//  ActorsSection - Takes the first four actors and passes them to RenderActor
-    // Will include two static render methods
-
 class APIService {
 
   static fetchMovie(movieId) {
@@ -37,20 +32,22 @@ class APIService {
 
   static fetchRandomMovie() {
     const movieId = Math.ceil(Math.random() * 10000);
-    let foundMovie;
     return APIService.fetchMovie(movieId)
-      .then(movie => {
-        console.log(movie.title);
-        if (!movie.title) {
-          this.fetchRandomMovie();
-        } else {
-          return movie;
-        }
-      })
+      // .then(movie => {
+      //   console.log(movie.title);
+      //   if (!movie.title) {
+      //     this.fetchRandomMovie();
+      //   } else {
+      //     return movie;
+      //   }
+      // })
   }
 
+  static TMDB_BASE_URL = 'https://api.themoviedb.org/3'        // full url for movie add 'movie/<movieId>?api_key=542003918769df50083a13c415bbc602'
+                                                            // full url for actors add 'movie/${movieId}/credits?api_key=542003918769df50083a13c415bbc602'
+
   static constructUrl(path) {
-    return `${TMDB_BASE_URL}/${path}?api_key=542003918769df50083a13c415bbc602`;
+    return `${this.TMDB_BASE_URL}/${path}?api_key=542003918769df50083a13c415bbc602`;
   }
 
   static fetchActors(movie) {
@@ -71,10 +68,13 @@ class Page {
 } 
 
 class Movie {
+  static BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780'  // full url add '/<backdrop_path>' (from movie JSON)
+
+
   constructor(json) {
     this.id = json.id
     this.title = json.title
-    this.backdropPath = BACKDROP_BASE_URL + json.backdrop_path
+    this.backdropPath = Movie.BACKDROP_BASE_URL + json.backdrop_path
     this.releaseDate = json.release_date
     this.overview = json.overview
     this.runtime = json.runtime + " minutes"
@@ -82,37 +82,43 @@ class Movie {
 }
 
 class Actor {
+  static PROFILE_BASE_URL = 'http://image.tmdb.org/t/p/w185'   // full url add '/<profile_path>' (from actor JSON)
+
   constructor(json) {
-    this.imageSrc = `${PROFILE_BASE_URL}/${json.profile_path}`
+    this.imageSrc = `${Actor.PROFILE_BASE_URL}/${json.profile_path}`
     this.name = json.name
   }
+
 }
 
+
+
 class MovieSection {
-  static renderMovie(movie) {
-    const movieBackdrop = document.getElementById('movie-backdrop');
-    const movieTitle = document.getElementById('movie-title');
-    const movieReleaseDate = document.getElementById('movie-release-date');
-    const movieRuntime = document.getElementById('movie-runtime');
-    const movieOverview = document.getElementById('movie-overview');
-  
-    movieBackdrop.src = movie.backdropPath;
-    movieTitle.innerHTML = movie.title;
-    movieReleaseDate.innerHTML = movie.releaseDate;
-    movieRuntime.innerHTML = movie.runtime;
-    movieOverview.innerHTML = movie.overview;
+  static movieBackdrop = document.getElementById('movie-backdrop');
+  static movieTitle = document.getElementById('movie-title');
+  static movieReleaseDate = document.getElementById('movie-release-date');
+  static movieRuntime = document.getElementById('movie-runtime');
+  static movieOverview = document.getElementById('movie-overview');
+
+  static renderMovie(movie) {  
+    this.movieBackdrop.src = movie.backdropPath;
+    this.movieTitle.innerHTML = movie.title;
+    this.movieReleaseDate.innerHTML = movie.releaseDate;
+    this.movieRuntime.innerHTML = movie.runtime;
+    this.movieOverview.innerHTML = movie.overview;
   }
   
 }
 
 class ActorsSection {
+  static actorList = document.getElementById('actors');
+  
   static renderActors(actors) {
     actors.forEach(actor => this.renderActor(actor));
   }
-  
+
   static renderActor(actor) {
-    const actorList = document.getElementById('actors');
-    actorList.insertAdjacentHTML('beforeend', `
+    this.actorList.insertAdjacentHTML('beforeend', `
       <li class = 'col-md-3'>
         <div class='row'>
           <img src="${actor.imageSrc}">
