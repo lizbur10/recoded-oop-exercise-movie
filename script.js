e10012d49793240ae44463952895f2fd253020f3
lizbur10 <liz.burton147@gmail.com>
2020-02-21 11:30:03 +0300

    clean up

--- script.js
+++ script.js
@@ -3,29 +3,28 @@
 //  call fetchActors from APP 
 //  create Actor model X
 //  add renderActors function to Page class X
-// add renderActor
+//  add renderActor
 
-
-const TMDB_BASE_URL = 'https://api.themoviedb.org/3'  //https://api.themoviedb.org/3/movie/${movieId}
-                                                      //https://api.themoviedb.org/3/movie/${movieId}/credits
-const PROFILE_BASE_URL = 'http://image.tmdb.org/t/p/w185'
-const BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780'
 
 class App {
   static run() {
     APIService.fetchMovie(534)
       .then(movie => {
         // add call to fetchActors, passing movie
-        Page.renderMovie(movie)
+        Page.renderMovieSection(movie)
         APIService.fetchActors(movie)
           .then(actors => {
             console.log(actors);
-            Page.renderActors(actors)
-      })})
+            Page.renderActorsSection(actors)
+        })
+      })
     }
 }
 
 class APIService {
+
+  static TMDB_BASE_URL = 'https://api.themoviedb.org/3'  //https://api.themoviedb.org/3/movie/${movieId}
+                                                      //https://api.themoviedb.org/3/movie/${movieId}/credits
 
   static fetchMovie(movieId) {
     const url = APIService._constructUrl(`movie/${movieId}`)
@@ -43,11 +42,21 @@ class APIService {
   }
 
   static  _constructUrl(path) {
-    return `${TMDB_BASE_URL}/${path}?api_key=${atob('NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=')}`
+    return `${this.TMDB_BASE_URL}/${path}?api_key=${atob('NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=')}`
   }
 }
 
 class Page {
+  static renderMovieSection(movie) {
+    MovieSection.renderMovie(movie)
+  }
+  static renderActorsSection(movie) {
+    ActorsSection.renderActors(movie)
+  }
+}
+
+class MovieSection {
+  static BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780'
   static backdrop = document.getElementById('movie-backdrop')
   static title = document.getElementById('movie-title')
   static releaseDate = document.getElementById('movie-release-date')
@@ -55,13 +64,15 @@ class Page {
   static overview = document.getElementById('movie-overview')
 
   static renderMovie(movie) {
-    Page.backdrop.src = BACKDROP_BASE_URL + movie.backdropPath
-    Page.title.innerText = movie.title
-    Page.releaseDate.innerText = movie.releaseDate
-    Page.runtime.innerText = movie.runtime + " minutes"
-    Page.overview.innerText = movie.overview
-  }
+    this.backdrop.src = MovieSection.BACKDROP_BASE_URL + movie.backdropPath
+    this.title.innerText = movie.title
+    this.releaseDate.innerText = movie.releaseDate
+    this.runtime.innerText = movie.runtime + " minutes"
+    this.overview.innerText = movie.overview
+  }  
+}
 
+class ActorsSection {
   static actorsList = document.getElementById('actors');
 
   static renderActors(actors) {
@@ -82,14 +93,17 @@ class Page {
           <h3>${actor.name}</h3>
         </div>
       </li>
-    `)  }
+    `)  
+  }
 }
 
 class Actor {
+  static PROFILE_BASE_URL = 'http://image.tmdb.org/t/p/w185'
+
   constructor(json) {
     //add code to create Actor
     this.name = json.name
-    this.profilePath = PROFILE_BASE_URL + json.profile_path 
+    this.profilePath = Actor.PROFILE_BASE_URL + json.profile_path 
   }
 }
 
