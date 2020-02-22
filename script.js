62c661363e93e1d4ddb9313fc19a0d6fa07605cf
lizbur10 <liz.burton147@gmail.com>
2020-02-22 13:29:08 +0300

    added home page

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
-// To add actors:
-//  add fetchActors to APIService X
-//  call fetchActors from APP 
-//  create Actor model X
-//  add renderActors function to Page class X
-//  add renderActor
-
-
 class App {
   static run() {
-    APIService.fetchMovie(534)
-      .then(movie => {
-        // add call to fetchActors, passing movie
-        Page.renderMovieSection(movie)
-        APIService.fetchActors(movie)
-          .then(actors => {
-            console.log(actors);
-            Page.renderActorsSection(actors)
-        })
-      })
-    }
+    // Call a method in APIService to fetch current movies
+    // Call renderMovies method
+    APIService.fetchMovies()
+    .then(movies => HomePage.renderMovies(movies))
+
+    // APIService.fetchMovie(534)
+    //   .then(movie => {
+    //     // add call to fetchActors, passing movie
+    //     MoviePage.renderMovieSection(movie)
+    //     APIService.fetchActors(movie)
+    //       .then(actors => {
+    //         console.log(actors);
+    //         MoviePage.renderActorsSection(actors)
+    //     })
+    //   })
+  }
 }
 
 class APIService {
 
   static TMDB_BASE_URL = 'https://api.themoviedb.org/3'  //https://api.themoviedb.org/3/movie/${movieId}
                                                       //https://api.themoviedb.org/3/movie/${movieId}/credits
+
+  // write a method to fetch current movies
+  // Use fetchActors as starter code
+  static fetchMovies() {
+    // write code to fetch the movies 
+    const url = APIService._constructUrl(`movie/now_playing`)
+    return fetch(url)
+      .then(res => res.json())
+      .then(json => json.results.map(movie => new Movie(movie))
+  )}
 
   static fetchMovie(movieId) {
     const url = APIService._constructUrl(`movie/${movieId}`)
@@ -46,7 +53,45 @@ class APIService {
   }
 }
 
-class Page {
+class HomePage {
+  //create renderMovies method
+  static container = document.getElementById('container');
+  static renderMovies(movies) {
+    movies.forEach(movie => {
+      const movieDiv = document.createElement("div");
+      const movieImage = document.createElement("img");
+      movieImage.src = `${movie.backdropUrl}`;
+      const movieTitle = document.createElement("h3");
+      movieTitle.textContent = `${movie.title}`;
+      movieImage.addEventListener("click", function() {
+        Movies.run(movie)
+      });
+
+
+      movieDiv.appendChild(movieTitle);
+      movieDiv.appendChild(movieImage);
+      this.container.appendChild(movieDiv);
+    })
+  }
+}
+
+class Movies {
+    static run(movie) {
+      APIService.fetchMovie(movie.id)
+      .then(movie => {
+        // add call to fetchActors, passing movie
+        MoviePage.renderMovieSection(movie)
+        APIService.fetchActors(movie)
+          .then(actors => {
+            console.log(actors);
+            MoviePage.renderActorsSection(actors)
+        })
+      })
+    }
+}
+
+class MoviePage {
+  // Change name to MoviePage
   static container = document.getElementById('container');
   static renderMovieSection(movie) {
     MovieSection.renderMovie(movie)
@@ -58,7 +103,7 @@ class Page {
 
 class MovieSection {
   static renderMovie(movie) {
-    Page.container.innerHTML = `
+    MoviePage.container.innerHTML = `
       <div class="row">
         <div class="col-md-4">
           <img id="movie-backdrop" src=${movie.backdropUrl}> 
@@ -88,7 +133,7 @@ class ActorsSection {
     const actorsContainer = document.createElement('ul');
     actorsContainer.setAttribute('class', "list-unstyled")
     actorsContainer.setAttribute('id', 'actors');
-    Page.container.appendChild(actorsContainer);
+    MoviePage.container.appendChild(actorsContainer);
       actors.forEach(actor => this.renderActor(actor,actorsContainer));
   }
 
@@ -120,6 +165,7 @@ class Movie {
   static BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780'
 
   constructor(json) {
+    
     this.id = json.id
     this.title = json.title
     this.releaseDate = json.release_date
